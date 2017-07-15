ee047b1d703c63a6c924e571aa6d447b13df943f
toriqpriad <toriqpriad@gmail.com>
2017-07-15 21:34:23 +0700

    15 juli 2017 : malam

--- application/views/admin/include/footer_menu.php
+++ application/views/admin/include/footer_menu.php
@@ -2,30 +2,21 @@
     <div class="container-fluid">
         <nav class="pull-left">
             <ul>
-
                 <li>
-                    <a href="http://www.creative-tim.com">
-                        Creative Tim
-                    </a>
-                </li>
-                <li>
-                    <a href="http://blog.creative-tim.com">
-                        Blog
-                    </a>
-                </li>
-                <li>
-                    <a href="http://www.creative-tim.com/license">
-                        Licenses
-                    </a>
+                    Halaman Administrator
                 </li>
             </ul>
         </nav>
         <div class="copyright pull-right">
-            &copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by <a href="http://www.creative-tim.com">Creative Tim</a>
+          <?php
+          if (isset($site)) {
+            echo $site->site_name;
+          }
+          ?> &copy; <script>document.write(new Date().getFullYear())</script>
         </div>
     </div>
 </footer>
 
 </div>
 </div>
-</body>  
+</body>
